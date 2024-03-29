0146401e49b25e176ef7d97fcf3f2e506bc433d6
TamNhien <canhthien91@gmail.com>
2024-03-29 19:38:52 +0700

    Add project files.

---
+++ P_StrNganNhatTrongLisr.py
@@ -0,0 +1,13 @@
+﻿def find_shortest_string(lst):
+    # Hàm này nhận vào một danh sách chuỗi và trả về chuỗi có độ dài ngắn nhất trong danh sách.
+    return min(lst, key=len)  # Sử dụng hàm min() với key=len để trực tiếp trả về chuỗi có độ dài ngắn nhất
+
+def main():
+    lst = [input(f"Nhập chuỗi thứ {i+1}: ") for i in range(int(input("Nhập số lượng chuỗi: ")))]  
+   
+    shortest_string = find_shortest_string(lst)  # Gọi hàm find_shortest_string() để tìm chuỗi ngắn nhất
+    print("Chuỗi ngắn nhất trong danh sách là:", shortest_string)
+
+if __name__ == "__main__":
+    main()
+ 
